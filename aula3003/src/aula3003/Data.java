cc535f47263ca641061d2fd40ea4cd981e8e8941
luiz.bello <luiz.bello@unoesc.edu.br>
2023-03-30 21:37:52 -0300

    aula 30/03

--- aula3003/src/aula3003/Data.java
+++ aula3003/src/aula3003/Data.java
@@ -1,5 +1,21 @@
 package aula3003;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Data {
 
+	
+    public static void main(String[] args) {
+        LocalDateTime dataHora = LocalDateTime.now();
+        String dataPorExtenso = getDataPorExtenso(dataHora);
+        System.out.println(dataPorExtenso);
+    }
+
+    public static String getDataPorExtenso(LocalDateTime dataHora) {
+        DateTimeFormatter formatoDataHora = DateTimeFormatter.ofPattern("EEEE, 'dia' dd 'de' MMMM 'de' yyyy 'e agora são' HH 'horas e' mm 'minutos.'", new Locale("pt", "BR"));
+        String dataPorExtenso = dataHora.format(formatoDataHora);
+        return "Hoje é " + dataPorExtenso;
+    }
 }
